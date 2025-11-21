29c6177cb35096fdfdcdad498f55aa0cfe263c88
sakdinun47152 <bwc47152@gmail.com>
2025-11-21 14:48:32 +0700

    Fix: Restore .gitignore and cleanup

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"COfgFs+3ng04w0p+mIjPOOC1BUmS3cszA2f4y0C49fg=\"\n}"
--- .next/dev/static/chunks/app_page_tsx_607b5035._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_ee64439a._.js",
-  "static/chunks/_67fad276._.js"
-],
-    source: "dynamic"
-});
